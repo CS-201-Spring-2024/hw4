6b73dc33ccb4fedb1b4b850e09a21f47e3343b54
Asad Tariq <asad.tariq@sse.habib.edu.pk>
2024-04-15 15:58:10 +0500

    Initial temple commit

---
+++ src/inverted_index.hpp
@@ -0,0 +1,14 @@
+#include "json.hpp"
+
+struct Posting {
+    std::string docId;
+    // add more attributes or methods as required
+};
+
+class InvertedIndex {
+    public:
+        InvertedIndex(std::string folderPath); // constructs an Inverted Index according to the given specifications
+        int getSize(); // returns the number of unique terms
+        void saveToJSON(); // saves the entire Index as a JSON file called <fileName>.json
+        std::vector<Posting> retrieve (std::string query); // takes a query string and returns a vector of postings matching the query sorted by score, from highest score to lowest score.
+};
